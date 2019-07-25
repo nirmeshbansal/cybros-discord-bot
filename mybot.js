3ffe3b235d440d3474d00ef27f3cecb797251017
Nirmesh Bansal <cerealkillercode@gmail.com>
2019-07-25 17:30:52 +0530

    Completed for Codechef

--- mybot.js
+++ mybot.js
@@ -7,14 +7,62 @@ client.on('ready', () => {
     client.user.setActivity("with Codechef")
 })
 
-PythonShell.run('scraper.py', null, function (err) {
-    if (err) throw err;
-    console.log('finished');
-  });
-
-
 var fs = require('fs');
-var objpresent = JSON.parse(fs.readFileSync('present.json', 'utf8'));
 
-console.log(objpresent.luls[0].contestcode);
+client.on('message', (receivedMessage) => {
+  if (receivedMessage.author == client.user) {
+    return
+  }
+  if (receivedMessage.content.startsWith("!")) {
+    processCommand(receivedMessage)
+  }
+})
+
+function processCommand(receivedMessage) {
+  let fullCommand = receivedMessage.content.substr(1);
+  if(fullCommand == "help") {
+    helpCommand(receivedMessage)
+  }
+  if(fullCommand == "pcontest") {
+    pcontest(receivedMessage)
+  }
+  if(fullCommand == "ucontest") {
+    ucontest(receivedMessage)
+  }
+
+}
+
+function helpCommand(receivedMessage) {
+  receivedMessage.channel.send("!info for Bot info\n!pcontest for information about ongoing contests\n!ucontest for information about upcoming contests\n!contest for information about all contests")
+}
+
+
+function pcontest(receivedMessage) {
+  PythonShell.run('scraper.py', null, function (err) {
+    if (err) throw err;
+  });
+  var objpresent = JSON.parse(fs.readFileSync('present.json', 'utf8'));
+  var tot = objpresent.luls.length;
+  for (i=0;i<tot;i++) {
+    receivedMessage.channel.send("\nName - " + objpresent.luls[i].contestname + "\nCode - " + objpresent.luls[i].contestcode + "\nStart - " + objpresent.luls[i].stime + "\nEnd - " + objpresent.luls[i].etime + "\n\n")
+    if(objpresent.luls[i] == null) {
+      break;
+    }
+  }
+}
+
+function ucontest(receivedMessage) {
+  PythonShell.run('scraper.py', null, function (err) {
+    if (err) throw err;
+  });
+  var objfuture = JSON.parse(fs.readFileSync('future.json', 'utf8'));
+  var tot2 = objfuture.luls.length;
+  for (i=0;i<tot2;i++) {
+    receivedMessage.channel.send("\nName - " + objfuture.luls[i].contestname + "\nCode - " + objfuture.luls[i].contestcode + "\nStart - " + objfuture.luls[i].stime + "\nEnd - " + objfuture.luls[i].etime + "\n\n")
+    if(objfuture.luls[i] == null) {
+      break;
+    }
+  }
+}
+
 client.login("NjAyNDkxNTk0NjE2MjA5NDA4.XTRu8Q.4gy7ClhT7H5V4eI1fyokgbH4ylU")
